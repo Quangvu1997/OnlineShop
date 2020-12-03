8f86096c9613a775c229fc5a8a9e2bb258c82852
Vũ Quang <nqvu270497@hotmail.com>
2020-12-03 22:16:24 +0700

    Add Model Entity

---
+++ OnlineShop.Model/Models/ProductTag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Model.Models
+{
+    [Table("ProductTags")]
+    public class ProductTag
+    {
+        [Key]
+        public int PoductID { set; get; }
+
+        [Key]
+        [Column(TypeName ="varchar")]
+        [MaxLength(50)]
+        public string TagID { set; get; }
+
+        [ForeignKey("ProductID")]
+        public virtual Product Product { set; get; }
+        [ForeignKey("TagID")]
+        public virtual Tag Tag { set; get; }
+    }
+}
